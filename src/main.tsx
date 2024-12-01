2a457a73e1d53e6fe3d91fad38140258214712db
Albert Chyb <albert7773000@gmail.com>
2024-12-01 17:15:09 +0100

    Setup react query and created a folder for initializing libraries

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { ProvideTanStackRouter } from '@/router/setup';
+import { ProvideTanStackRouter } from '@/init/router.tsx';
+import { ProvideTanStackQuery } from '@/init/tanstack-query.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ProvideTanStackRouter />
+    <ProvideTanStackQuery>
+      <ProvideTanStackRouter />
+    </ProvideTanStackQuery>
   </StrictMode>,
 );
