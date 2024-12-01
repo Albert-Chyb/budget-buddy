261062124b2d6c93e71ffedad7c00895eddae16b
Albert Chyb <albert7773000@gmail.com>
2024-12-01 18:46:47 +0100

    Setup supabase client

---
+++ src/init/supabase.tsx
@@ -0,0 +1,28 @@
+import { createClient } from '@supabase/supabase-js';
+import { createContext, PropsWithChildren, useContext } from 'react';
+
+const supabaseClient = createClient(
+  import.meta.env.VITE_SUPABASE_PROJECT_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+);
+
+const supabaseContext = createContext(supabaseClient);
+
+export function useSupabase() {
+  const supabase = useContext(supabaseContext);
+
+  if (!supabase)
+    throw new Error(
+      'Supabase context can only be used inside a supabase provider',
+    );
+
+  return supabase;
+}
+
+export function ProvideSupabase({ children }: PropsWithChildren) {
+  return (
+    <supabaseContext.Provider value={supabaseClient}>
+      {children}
+    </supabaseContext.Provider>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { ProvideTanStackRouter } from '@/init/router.tsx';
 import { ProvideTanStackQuery } from '@/init/tanstack-query.tsx';
+import { ProvideSupabase } from '@/init/supabase.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ProvideTanStackQuery>
-      <ProvideTanStackRouter />
-    </ProvideTanStackQuery>
+    <ProvideSupabase>
+      <ProvideTanStackQuery>
+        <ProvideTanStackRouter />
+      </ProvideTanStackQuery>
+    </ProvideSupabase>
   </StrictMode>,
 );
