ec72a9c87b94a774bf158efff12cf133efc6da18
Albert Chyb <albert7773000@gmail.com>
2024-12-04 15:09:47 +0100

    Created layout for sign-in page

--- src/routes/sign-in.tsx
+++ src/routes/sign-in.tsx
@@ -1,9 +1,38 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/card.tsx';
 
 export const Route = createFileRoute('/sign-in')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/sign-in"!</div>;
+  return (
+    <Card className='max-w-screen-sm mx-auto'>
+      <CardHeader>
+        <CardTitle>Zaloguj się</CardTitle>
+        <CardDescription>
+          Wypełnij ponizszy formularz, aby zalogować sie na istniejące konto.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>Formularz</CardContent>
+      <CardFooter className='justify-center'>
+        <p className='typography-muted'>
+          Nie posiadasz jeszcze konta ?{' '}
+          <Link
+            to='/sign-up'
+            className='typography-link'
+          >
+            Załóż konto
+          </Link>
+        </p>
+      </CardFooter>
+    </Card>
+  );
 }
