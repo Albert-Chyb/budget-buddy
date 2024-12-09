3a4ab36579bdee19f33d7952754fdce7aed861cd
Albert Chyb <albert7773000@gmail.com>
2024-12-09 13:30:21 +0100

    Changed redirect link in emails with reset password instructions

--- src/auth/reset-password/reset-password-mutation.ts
+++ src/auth/reset-password/reset-password-mutation.ts
@@ -1,12 +1,18 @@
 import { useSupabase } from '@/init/supabase.tsx';
 import { useMutation } from '@tanstack/react-query';
+import { linkOptions } from '@tanstack/react-router';
+
+const changePasswordRoute = linkOptions({ to: '/change-password' }).to;
+const REDIRECT_URL = new URL(changePasswordRoute, location.origin);
 
 export function useResetPasswordMutation() {
   const supabase = useSupabase();
 
   return useMutation({
     mutationFn: async (email: string) => {
-      const { error } = await supabase.auth.resetPasswordForEmail(email);
+      const { error } = await supabase.auth.resetPasswordForEmail(email, {
+        redirectTo: REDIRECT_URL.href,
+      });
 
       if (error) throw error;
     },
