b57c40408c0e6cd9dd1ade79f0951313f002c849
Albert Chyb <albert7773000@gmail.com>
2024-12-13 12:16:18 +0100

    Setup devtools for router and tanstack query

--- src/init/router.tsx
+++ src/init/router.tsx
@@ -1,12 +1,28 @@
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from '@/route-tree.gen.ts';
+import { lazy, Suspense } from 'react';
 
 const router = createRouter({
   routeTree,
 });
 
+const DevTools = import.meta.env.DEV
+  ? lazy(() =>
+      import('@tanstack/router-devtools').then((res) => ({
+        default: res.TanStackRouterDevtools,
+      })),
+    )
+  : () => null;
+
 function ProvideTanStackRouter() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Suspense>
+        <DevTools router={router} />
+      </Suspense>
+    </>
+  );
 }
 
 declare module '@tanstack/react-router' {
--- src/init/tanstack-query.tsx
+++ src/init/tanstack-query.tsx
@@ -1,8 +1,23 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { PropsWithChildren } from 'react';
+import { lazy, PropsWithChildren, Suspense } from 'react';
 
 const client = new QueryClient();
 
+const DevTools = import.meta.env.DEV
+  ? lazy(() =>
+      import('@tanstack/react-query-devtools').then((res) => ({
+        default: res.ReactQueryDevtools,
+      })),
+    )
+  : () => null;
+
 export function ProvideTanStackQuery({ children }: PropsWithChildren) {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <Suspense>
+        <DevTools />
+      </Suspense>
+    </QueryClientProvider>
+  );
 }
