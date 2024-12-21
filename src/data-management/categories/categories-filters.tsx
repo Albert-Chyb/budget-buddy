85c885ffd09e8effa0faf039c9e9ee2d5b5f98f3
Albert Chyb <albert7773000@gmail.com>
2024-12-21 15:29:34 +0100

    Added a button that clears all column filters

--- src/data-management/categories/categories-filters.tsx
+++ src/data-management/categories/categories-filters.tsx
@@ -1,14 +1,11 @@
-import { Filters } from '@/data-management/filters.tsx';
+import { Filters, FiltersProps } from '@/data-management/filters.tsx';
 import { CategoryNameFilter } from '@/data-management/categories/category-name-filter.tsx';
 import { CategoryTypeFilter } from '@/data-management/categories/category-type-filter.tsx';
 import { CategoryColorFilter } from '@/data-management/categories/category-color-filter.tsx';
-import { Table } from '@tanstack/react-table';
 import { safelyGetTableColumn } from '@/helpers/safely-get-table-column.ts';
 import { CategoriesTableColumnsId } from '@/data-management/categories/categories-table-columns.tsx';
 
-interface CategoriesFiltersProps {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  table: Table<any>;
+interface CategoriesFiltersProps extends FiltersProps {
   colors: string[];
 }
 
@@ -16,7 +13,7 @@ export const CategoriesFilters = (props: CategoriesFiltersProps) => {
   const { colors, table } = props;
 
   return (
-    <Filters>
+    <Filters table={table}>
       <CategoryNameFilter
         column={safelyGetTableColumn(table, CategoriesTableColumnsId.Name)}
       />
--- src/data-management/filters.tsx
+++ src/data-management/filters.tsx
@@ -1,16 +1,34 @@
 import { PropsWithChildren } from 'react';
+import { Button } from '@/components/button.tsx';
+import { FilterX } from 'lucide-react';
+import { Table } from '@tanstack/react-table';
 
-export function Filters({ children }: PropsWithChildren) {
+export interface FiltersProps extends PropsWithChildren {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  table: Table<any>;
+}
+
+export function Filters(props: FiltersProps) {
+  const { children, table } = props;
   return (
     <section>
       <h2 className='typography-large mb-2'>Filtry</h2>
 
-      <ul
-        className='flex gap-4'
-        aria-label='Lista filtrów'
-      >
-        {children}
-      </ul>
+      <div className='flex gap-2 flex-wrap'>
+        <ul
+          className='flex gap-4'
+          aria-label='Lista filtrów'
+        >
+          {children}
+        </ul>
+
+        <Button
+          className='ml-auto'
+          onClick={() => table.resetColumnFilters()}
+        >
+          <FilterX /> Wyczyść filtry
+        </Button>
+      </div>
     </section>
   );
 }
