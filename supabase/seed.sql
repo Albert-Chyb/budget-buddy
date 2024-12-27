7c606e94451fecf146e331bec3aea37606673011
Albert Chyb <albert7773000@gmail.com>
2024-12-27 14:08:07 +0100

    Created categories table and its sub-tables

---
+++ supabase/seed.sql
@@ -0,0 +1,22 @@
+INSERT INTO category_colors (red, green, blue, name)
+VALUES (239, 068, 068, 'Czerwony'),
+       (249, 115, 022, 'Pomarańczowy'),
+       (245, 158, 011, 'Bursztynowy'),
+       (234, 179, 008, 'Żółty'),
+       (132, 204, 022, 'Limonkowy'),
+       (034, 197, 094, 'Zielony'),
+       (016, 054, 255, 'Szmaragdowy'),
+       (020, 177, 240, 'Turkusowy'),
+       (012, 098, 208, 'Cyjanowy'),
+       (023, 165, 233, 'Błękitny'),
+       (059, 130, 246, 'Niebieski'),
+       (099, 102, 241, 'Indygo'),
+       (139, 092, 246, 'Fioletowy'),
+       (168, 085, 247, 'Purpurowy'),
+       (217, 070, 239, 'Fuksjowy'),
+       (236, 072, 153, 'Malinowy'),
+       (244, 063, 094, 'Karmazynowy');
+
+INSERT INTO category_types (name, is_expense)
+VALUES ('Wydatek', true),
+       ('Przychód', false);
