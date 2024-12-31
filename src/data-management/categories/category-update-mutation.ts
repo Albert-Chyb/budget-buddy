97d8f178fb76eb03f7e2ac2f9268e63efddd4e41
Albert Chyb <albert7773000@gmail.com>
2024-12-31 16:35:51 +0100

    Added owner_id column to the query filters

--- src/data-management/categories/category-update-mutation.ts
+++ src/data-management/categories/category-update-mutation.ts
@@ -2,21 +2,27 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useSupabase } from '@/init/supabase.tsx';
 import { TablesUpdate } from '@/database/types.ts';
 import { CATEGORIES_TABLE_DATA_QUERY_KEY } from '@/data-management/categories/categories-table-data-query.ts';
+import { useUserQuery } from '@/auth/user-query.ts';
 
 type RequireRecordId = { id: number };
 
 export const useCategoryUpdateMutation = () => {
   const supabase = useSupabase();
   const queryClient = useQueryClient();
+  const { data: user } = useUserQuery();
 
   return useMutation({
     mutationFn: async (
       category: Omit<TablesUpdate<'categories'>, 'id'> & RequireRecordId,
     ) => {
+      if (!user)
+        throw new Error('CategoryUpdateMutation requires user to be logged in');
+
       const { error } = await supabase
         .from('categories')
         .update(category)
-        .eq('id', category.id);
+        .eq('id', category.id)
+        .eq('owner_id', user.id);
 
       if (error) throw error;
     },
