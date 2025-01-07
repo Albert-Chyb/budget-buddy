45d1915e2de701ad4482c2e284cc2bf0b47d61e8
Albert Chyb <albert7773000@gmail.com>
2025-01-07 17:24:27 +0100

    Adjusted creator trigger for mobile screen sizes

--- src/data-management/categories/data-mutation/category-creator.tsx
+++ src/data-management/categories/data-mutation/category-creator.tsx
@@ -1,15 +1,22 @@
 import { Editor } from '@/data-management/data-mutation/editor.tsx';
-import { Button } from '@/components/button.tsx';
 import {
   CreateCategoryForm,
   CreateCategoryFormProps,
 } from '@/data-management/categories/data-mutation/forms/create-category-form.tsx';
+import { ResponsiveButton } from '@/data-management/responsive-button.tsx';
+import { CirclePlus } from 'lucide-react';
 
 export type CategoryCreatorProps = CreateCategoryFormProps & { id: string };
 
 export function CategoryCreator(props: CategoryCreatorProps) {
   const { id, ...formProps } = props;
-  const trigger = <Button variant='secondary'>Nowa kategoria</Button>;
+  const trigger = (
+    <ResponsiveButton
+      variant='secondary'
+      icon={<CirclePlus />}
+      label='Nowa kategoria'
+    />
+  );
   const form = <CreateCategoryForm {...formProps} />;
 
   return (
---
+++ src/data-management/responsive-button.tsx
@@ -0,0 +1,46 @@
+import {
+  ComponentPropsWithoutRef,
+  ComponentRef,
+  ForwardedRef,
+  forwardRef,
+  ReactNode,
+} from 'react';
+import { useIsMobile } from '@/data-management/is-mobile.ts';
+import { Button } from '@/components/button.tsx';
+
+export interface ResponsiveButtonProps
+  extends ComponentPropsWithoutRef<typeof Button> {
+  icon: ReactNode;
+  label: string;
+}
+
+export const ResponsiveButton = forwardRef(
+  (
+    props: ResponsiveButtonProps,
+    forwardedRef: ForwardedRef<ComponentRef<typeof Button>>,
+  ) => {
+    const { icon, label, ...btnProps } = props;
+    const isMobile = useIsMobile();
+
+    if (isMobile)
+      return (
+        <Button
+          aria-label={label}
+          ref={forwardedRef}
+          size='icon'
+          {...btnProps}
+        >
+          {icon}
+        </Button>
+      );
+
+    return (
+      <Button
+        ref={forwardedRef}
+        {...btnProps}
+      >
+        {label}
+      </Button>
+    );
+  },
+);
