38fd06fee99804013d5d6ef2d3e9cdf7a58d37ec
Albert Chyb <albert7773000@gmail.com>
2025-01-12 13:01:14 +0100

    Deleted abstract table hook

--- src/data-management/categories/data-view/categories-table-model.ts
+++ src/data-management/categories/data-view/categories-table-model.ts
@@ -1,19 +1,46 @@
 import { categoriesTableColumns } from '@/data-management/categories/data-view/categories-table-columns.tsx';
-import { useDataManagementTable } from '@/data-management/data-management-table-hook.ts';
 import { useMemo } from 'react';
 import { CategoryRowData } from '@/database/categories/table-data-query.ts';
 import { CategoryColor } from '@/database/category-colors/query.ts';
 import { CategoryType } from '@/database/category-types/query.ts';
+import { usePaginationState } from '@/data-management/pagination/pagination-state.ts';
+import { useSortingState } from '@/data-management/sorting/sorting-state.ts';
+import { useColumnFiltersState } from '@/data-management/filtering/column-filters-state.ts';
+import {
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 
 export const useCategoriesTable = (
   categories: CategoryRowData[],
   categoryTypes: CategoryType[],
   categoryColors: CategoryColor[],
 ) => {
+  const [pagination, handlePaginationChange] = usePaginationState();
+  const [sorting, setSorting] = useSortingState();
+  const [columnFilters, setColumnFilters] = useColumnFiltersState();
   const columns = useMemo(
     () => categoriesTableColumns(categoryTypes, categoryColors),
     [categoryColors, categoryTypes],
   );
 
-  return useDataManagementTable(columns, categories);
+  return useReactTable({
+    columns,
+    data: categories,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    state: {
+      pagination,
+      sorting,
+      columnFilters,
+    },
+    onPaginationChange: handlePaginationChange,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+  });
 };
