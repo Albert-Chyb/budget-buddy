563c0526a85f2e5751a820ed5c95d9826231588d
Albert Chyb <albert7773000@gmail.com>
2025-01-17 16:38:49 +0100

    Added descriptions of individual app features

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -17,51 +17,93 @@ function RouteComponent() {
     <>
       <h1 className='typography-large mb-4'>Zarządzaj danymi</h1>
 
-      <div className='grid grid-cols-4 gap-2'>
-        <section>
-          <Card>
-            <CardHeader>
-              <CardTitle>
-                <h2>Kategorie</h2>
-              </CardTitle>
-              <CardDescription>
-                <p>Przeglądaj i edytuj kategorie transakcji.</p>
-              </CardDescription>
-            </CardHeader>
+      <ul
+        aria-label='Lista funkcji'
+        className='grid gap-2'
+        style={{
+          gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 600px))',
+        }}
+      >
+        <li>
+          <section>
+            <Card>
+              <CardHeader>
+                <CardTitle>
+                  <h2>Kategorie</h2>
+                </CardTitle>
+                <CardDescription>
+                  <p>
+                    Kategorie w dzienniku wydatków służą do przypisywania
+                    wydatków do określonych grup tematycznych, co pozwala na
+                    lepsze zrozumienie, jak rozkładają się Twoje finanse.
+                    Użytkownik może tworzyć własne kategorie, dostosowane do
+                    swoich potrzeb. Dzięki temu:
+                  </p>
+                  <ul className='typography-list'>
+                    <li>
+                      Możesz łatwo zobaczyć, ile wydajesz na poszczególne
+                      aspekty życia.
+                    </li>
+                    <li>
+                      Analiza danych finansowych staje się bardziej intuicyjna
+                      dzięki podziałowi na segmenty.
+                    </li>
+                  </ul>
+                </CardDescription>
+              </CardHeader>
 
-            <CardFooter className='justify-end'>
-              <Link
-                to='/categories'
-                className={buttonVariants({ variant: 'ghost' })}
-              >
-                Przejdź do strony
-              </Link>
-            </CardFooter>
-          </Card>
-        </section>
+              <CardFooter className='justify-end'>
+                <Link
+                  to='/categories'
+                  className={buttonVariants({ variant: 'link' })}
+                >
+                  Zarządzaj kategoriami
+                </Link>
+              </CardFooter>
+            </Card>
+          </section>
+        </li>
 
-        <section>
-          <Card>
-            <CardHeader>
-              <CardTitle>
-                <h2>Portfele</h2>
-              </CardTitle>
-              <CardDescription>
-                <p>Przeglądaj i edytuj swoje portfele.</p>
-              </CardDescription>
-            </CardHeader>
+        <li>
+          <section>
+            <Card>
+              <CardHeader>
+                <CardTitle>
+                  <h2>Portfele</h2>
+                </CardTitle>
+                <CardDescription>
+                  <p>
+                    Portfele reprezentują różne źródła lub miejsca
+                    przechowywania środków finansowych, takie jak gotówka, konto
+                    bankowe, karta kredytowa czy portfel cyfrowy. Każdy portfel
+                    pozwala na śledzenie przepływów finansowych w konkretnym
+                    kanale, co pomaga w:
+                  </p>
+                  <ul className='typography-list'>
+                    <li>
+                      Monitorowaniu salda i transakcji dla każdego portfela z
+                      osobna.
+                    </li>
+                    <li>
+                      Analizie, z którego źródła najczęściej finansowane są
+                      poszczególne wydatki.
+                    </li>
+                  </ul>
+                </CardDescription>
+              </CardHeader>
 
-            <CardFooter className='justify-end'>
-              <Link
-                to='/wallets'
-                className={buttonVariants({ variant: 'ghost' })}
-              >
-                Przejdź do strony
-              </Link>
-            </CardFooter>
-          </Card>
-        </section>
-      </div>
+              <CardFooter className='justify-end'>
+                <Link
+                  to='/categories'
+                  className={buttonVariants({ variant: 'link' })}
+                >
+                  Zarządzaj portfelami
+                </Link>
+              </CardFooter>
+            </Card>
+          </section>
+        </li>
+      </ul>
     </>
   );
 }
