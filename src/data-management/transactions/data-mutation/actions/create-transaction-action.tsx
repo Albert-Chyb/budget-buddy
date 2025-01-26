8658b478926d1172a51acd3ad5aa0a079048f6ca
Albert Chyb <albert7773000@gmail.com>
2025-01-26 13:52:41 +0100

    Placed transaction creator inside the create action

--- src/data-management/transactions/data-mutation/actions/create-transaction-action.tsx
+++ src/data-management/transactions/data-mutation/actions/create-transaction-action.tsx
@@ -1,10 +1,13 @@
 import { useCreateTransactionMutation } from '@/database/transactions/create-transaction-mutation.ts';
-import { TransactionCreator } from '@/data-management/transactions/data-mutation/transaction-creator.tsx';
 import { WalletsListQueryData } from '@/database/wallets/wallets-list-query.ts';
 import { CategoriesListQueryData } from '@/database/categories/categories-list-query.ts';
 import { MutationErrorDialog } from '@/data-management/data-mutation/mutation-error-dialog.tsx';
 import { useEditorContext } from '@/data-management/data-mutation/editor-open-state.tsx';
 import { TransactionFormValue } from '@/data-management/transactions/data-mutation/forms/form-schemas/transaction-form-schema.ts';
+import { ResponsiveButton } from '@/data-management/responsive-button.tsx';
+import { Plus } from 'lucide-react';
+import { TransactionForm } from '@/data-management/transactions/data-mutation/forms/transaction-form.tsx';
+import { Editor } from '@/data-management/data-mutation/editor.tsx';
 
 export interface CreateTransactionActionProps {
   wallets: WalletsListQueryData;
@@ -35,12 +38,30 @@ export const CreateTransactionAction = ({
         />
       )}
 
-      <TransactionCreator
-        wallets={wallets}
-        categories={categories}
-        isPending={isPending}
-        onSubmit={handleSubmit}
-      />
+      <Editor
+        id='transaction-creator'
+        isDismissible={!isPending}
+      >
+        {{
+          title: 'Nowa transakcja',
+          description:
+            'Po wypełnieniu formularza wyślij go, aby dodać nową transakcje',
+          trigger: (
+            <ResponsiveButton
+              icon={<Plus />}
+              label='Nowa transakcja'
+            />
+          ),
+          form: (
+            <TransactionForm
+              wallets={wallets}
+              categories={categories}
+              isPending={isPending}
+              onSubmit={handleSubmit}
+            />
+          ),
+        }}
+      </Editor>
     </>
   );
 };
