bbaa5633b3498551fa4456b5422c410c710e8df4
Albert Chyb <albert7773000@gmail.com>
2025-02-23 12:39:47 +0100

    Removed unused code

--- src/helpers/prev-value-monitor.ts
+++
@@ -1,7 +0,0 @@
-import { useEffect, useState } from 'react';
-
-export function usePrevValueMonitor(isDismissible: boolean) {
-  const [prev, setPrev] = useState<boolean | null>(null);
-  useEffect(() => () => setPrev(isDismissible), [isDismissible]);
-  return prev;
-}
