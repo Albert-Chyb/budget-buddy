5d9326e53f24e55dc8f9dc58c9445a429dbd5105
Albert Chyb <albert7773000@gmail.com>
2025-03-01 19:18:11 +0100

    Used variables to store newly created records ids

--- supabase/tests/database/functions/get_transactions_summary.sql
+++ supabase/tests/database/functions/get_transactions_summary.sql
@@ -4,18 +4,28 @@ LANGUAGE PLPGSQL
 AS $$
 DECLARE
     var_user_id UUID;
+    var_incomes_category_type_id category_types.id%TYPE;
+    var_expenses_category_type_id category_types.id%TYPE;
+    var_incomes_category_id categories.id%TYPE;
+    var_expenses_category_id categories.id%TYPE;
+    var_wallet_id wallets.id%TYPE;
 BEGIN
     INSERT INTO auth.users (id, email) VALUES (gen_random_uuid(), 'unit_tests@domain.com') RETURNING id INTO var_user_id;
-    INSERT INTO category_types(id, name, is_expense) VALUES (100, 'Fake', FALSE), (101, 'Fake', TRUE);
-    INSERT INTO categories(id, name, type_id, owner_id) VALUES (100, 'Fake', 100, var_user_id), (101, 'Fake', 101, var_user_id);
-    INSERT INTO wallets(id, name, balance, owner_id) VALUES (100, 'Fake', 10000, var_user_id);
 
-    INSERT INTO transactions(id, wallet_id, category_id, amount, owner_id) VALUES
-        (100, 100, 100, 1, var_user_id),
-        (101, 100, 100, 2, var_user_id),
-        (102, 100, 101, 3, var_user_id),
-        (103, 100, 101, 4, var_user_id);
-    execute format('set request.jwt.claim.sub=%L', var_user_id);
+    INSERT INTO category_types(name, is_expense) VALUES ('Fake', FALSE) RETURNING id INTO var_incomes_category_type_id;
+    INSERT INTO category_types(name, is_expense) VALUES ('Fake', TRUE) RETURNING id INTO var_expenses_category_type_id;
+    
+    INSERT INTO categories(name, type_id, owner_id) VALUES ('Fake', var_incomes_category_type_id, var_user_id) RETURNING id INTO var_incomes_category_id;
+    INSERT INTO categories(name, type_id, owner_id) VALUES ('Fake', var_expenses_category_type_id, var_user_id) RETURNING id INTO var_expenses_category_id;
+    
+    INSERT INTO wallets(name, balance, owner_id) VALUES ('Fake', 10000, var_user_id) RETURNING id INTO var_wallet_id;
+
+    INSERT INTO transactions(wallet_id, category_id, amount, owner_id) VALUES
+        (var_wallet_id, var_incomes_category_id, 1, var_user_id),
+        (var_wallet_id, var_incomes_category_id, 2, var_user_id),
+        (var_wallet_id, var_expenses_category_id, 3, var_user_id),
+        (var_wallet_id, var_expenses_category_id, 4, var_user_id);
+    EXECUTE format('set request.jwt.claim.sub=%L', var_user_id);
 
     RETURN NEXT results_eq(
         'SELECT is_expense, sum FROM get_transactions_summary() ORDER BY is_expense',
